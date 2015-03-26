346bbec0ceebf7bca94b7b1ecfb068a669eff932
Seth Johnson <sethjdesigns@gmail.com>
2015-03-26 09:41:02 -0400

    first method created

--- FunWithFunctions/Program.cs
+++ FunWithFunctions/Program.cs
@@ -10,6 +10,16 @@ namespace FunWithFunctions
     {
         static void Main(string[] args)
         {
+            var elapsedDays = ComputeDays(DateTime.Now, new DateTime(2015, 3, 23));
+            Console.WriteLine("I've been writing software for {0} days", elapsedDays);
+            Console.ReadLine();
+        }
+
+        public static int ComputeDays(DateTime endPoint, DateTime startPoint)
+        {
+            var days = endPoint - startPoint;
+            var elapsedDays = Math.Floor(days.TotalDays);
+            return (int)elapsedDays;
         }
     }
 }
