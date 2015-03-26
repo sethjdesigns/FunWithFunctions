e2ffc82f07c84618517232b4044ce8a8a9bae4a1
Seth Johnson <sethjdesigns@gmail.com>
2015-03-26 10:25:12 -0400

    Bools!

--- FunWithFunctions/Program.cs
+++ FunWithFunctions/Program.cs
@@ -10,9 +10,61 @@ namespace FunWithFunctions
     {
         static void Main(string[] args)
         {
+            bool test = false;
+            test = true;
+
+            bool? test2 = default(bool?);
+            test2 = true;
+            test2 = false;
+
+            BoolTest(test2);
+            test2 = true;
+            BoolTest(test2);
+            test2 = false;
+            BoolTest(test2);
+
             var elapsedDays = ComputeDays(DateTime.Now, new DateTime(2015, 3, 23));
             Console.WriteLine("I've been writing software for {0} days", elapsedDays);
-            Console.ReadLine();
+
+            elapsedDays = ComputeDays(new DateTime(2015, 5, 15), new DateTime(2015, 3, 23));
+            Console.WriteLine("When I finish, I will have written software for {0} days", elapsedDays);
+
+            elapsedDays = ComputeDays(DateTime.Now, new DateTime(1999, 12, 31));
+            Console.WriteLine("We have not been able to party like it's 1999, for {0} days", elapsedDays);
+
+            int factorial = Factorial(10);
+            Console.WriteLine(factorial);
+
+            factorial = RecursiveFactorial(10);
+            Console.WriteLine(factorial);
+
+            Console.ReadLine(); 
+        }
+
+        private static void BoolTest(bool? test2)
+        {
+            if (test2 == true)
+                Console.WriteLine("Tru");
+            else if (test2 == false)
+                Console.WriteLine("False");
+            else if (!test2.HasValue)
+                Console.WriteLine("has no value");
+            else
+                Console.WriteLine("NOPE");
+        }
+
+        private static int RecursiveFactorial(int p)
+        {
+            return (p == 1) ? 1 : p * RecursiveFactorial(p - 1);
+        }
+
+        public static int Factorial(int n) 
+        {
+            var answer = 1;
+            for (int i = 1; i <= n; i++)
+                answer *= i;
+
+            return answer;
         }
 
         public static int ComputeDays(DateTime endPoint, DateTime startPoint)
